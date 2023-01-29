e27064c8e00cf5401b8908c00581c4c859e3723d
Noah Hefner <noah.hefner127@gmail.com>
2023-01-28 22:43:17 -0500

    script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./setup.sh" 2>&1
+  exit 1
+fi
+
+# Install stow
+apt install stow
+
+# Stow
+stow --adopt bash
+stow --adopt nvim
